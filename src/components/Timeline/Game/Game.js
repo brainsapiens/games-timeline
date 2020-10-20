130a7b93452ab21260ee5bb33de981b220b0293f
Vyacheslav Efremenko <brainsapiens@gmail.com>
2020-10-20 22:22:28 +0300

    Refactoring in progress

--- src/components/Timeline/Game/Game.js
+++ src/components/Timeline/Game/Game.js
@@ -1,6 +1,9 @@
 import React, {Component} from 'react';
 import styled from 'styled-components';
 
+import iconLink from '../../../images/icons/link.svg';
+import iconLinkOff from '../../../images/icons/link-off.svg';
+
 // Styles
 const releaseUnknownGameTitle = `
     .game-title {
@@ -66,6 +69,33 @@ const Game = styled.article`
         }
     }
 `;
+const Anchor = styled.a`
+    position: absolute;
+    top: 0;
+    right: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100%;
+    padding: .6rem;
+    background-color: rgba(255, 255, 255, .75);
+    color: #000;
+    opacity: 0;
+    transition: opacity var(--transition-duration-base);
+
+    &:hover > img {
+        opacity: .75;
+    }
+
+    > img {
+        transition: opacity var(--transition-duration-base);
+    }
+
+    ${Game}:hover &,
+    ${Game}.anchor & {
+        opacity: 1;
+    }
+`;
 const Title = styled.h2`
     color: #fff;
     font-size: 1.3rem;
@@ -105,6 +135,85 @@ const Footer = styled.footer`
 `;
 
 class TimelineGame extends Component {
+    constructor (props) {
+        super(props);
+
+        this.state = {
+            anchor: false
+        }
+
+        // this.toggleAnchor = this.toggleAnchor.bind(this);
+    }
+
+    componentDidMount () {
+        this.setAnchor();
+    }
+
+    // Anchor
+    matchAnchorToHash = () => {
+        return this.anchorUrl && `#${this.anchorUrl}` === document.location.hash;
+    }
+
+    setAnchor = () => {
+        const hash = document.location.hash;
+        const hashValue = hash.substring(1);
+        const item = document.querySelector('[data-anchor="' + hashValue + '"]');
+
+        if (item) {
+            item.scrollIntoView({
+                behavior: 'auto',
+                block: 'center',
+                inline: 'center'
+            });
+
+            item.querySelector('.game-title > a').focus();
+        }
+
+        if (this.matchAnchorToHash()) {
+            this.setState({anchor: true});
+        }
+    }
+    // toggleAnchor = event => {
+    //     const item = document.querySelector('[data-anchor="' + this.anchorUrl + '"]');
+    //
+    //     if (item) {
+    //         if (this.matchAnchorToHash()) {
+    //             event.preventDefault();
+    //
+    //             this.setState({'anchor': false});
+    //
+    //             window.history.pushState('', document.title, window.location.pathname);
+    //         } else {
+    //             this.setState({'anchor': true});
+    //         }
+    //     }
+    // }
+
+    get anchorUrl () {
+        const {url, release} = this.props.game;
+
+        if (url && release) {
+            return url.replace('https://en.wikipedia.org/wiki/', '');
+        }
+
+        return null;
+    }
+    get anchor () {
+        const icon = this.state.anchor ? iconLinkOff : iconLink;
+        const {url, release} = this.props.game;
+
+        return (url && release) ? (
+            <Anchor
+                href={`#${this.anchorUrl}`}
+                title='Game anchor'
+                // onClick={this.toggleAnchor}
+            >
+                <img src={icon} width='24' height='24' alt='anchor' />
+            </Anchor>
+        ) : null
+    }
+
+    // Game
     get title () {
         const {title, url} = this.props.game;
 
@@ -128,14 +237,18 @@ class TimelineGame extends Component {
 
     render () {
         const {release, expansion} = this.props.game;
+        const {anchor} = this.state;
 
         return (
             <Game
                 className={[
                     expansion ? 'expansion' : '',
-                    !release ? 'release-unknown' : ''
+                    !release ? 'release-unknown' : '',
+                    anchor ? 'anchor' : '',
                 ]}
+                data-anchor={this.anchorUrl}
             >
+                {this.anchor}
                 {this.title}
                 {this.footer}
             </Game>
--- src/components/Timeline/Games/Games.js
+++ src/components/Timeline/Games/Games.js
@@ -3,9 +3,6 @@ import styled from 'styled-components';
 
 import games from '../../../data/games.json'
 
-import iconLink from '../../../images/icons/link.svg';
-import iconLinkOff from '../../../images/icons/link-off.svg';
-
 import Game from '../Game';
 
 // Styles
@@ -15,124 +12,13 @@ const List = styled.ol`
     grid-gap: .8rem;
 `;
 const Item = styled.li`
-    position: relative;
-
     &.release-unknown {
         flex: 0 0 100%;
-    }
-`;
-const Anchor = styled.a`
-    position: absolute;
-    top: 0;
-    right: 100%;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    height: 100%;
-    padding: .6rem;
-    background-color: rgba(255, 255, 255, .75);
-    color: #000;
-    opacity: 0;
-    transition: opacity var(--transition-duration-base);
-    
-    &:hover > img {
-        opacity: .75;
-    }
-    
-    > img {
-        transition: opacity var(--transition-duration-base);
-    }
-    
-    ${Item}:hover &,
-    ${Item}.anchor & {
-        opacity: 1;
     }
 `;
 
 // Component
 class TimelineGames extends Component {
-    constructor (props) {
-        super(props);
-
-        this.state = {
-            anchor: false
-        }
-
-        this.toggleAnchor = this.toggleAnchor.bind(this);
-    }
-
-    game = {
-        url: null,
-        release: null
-    };
-
-    componentDidMount () {
-        this.setAnchor();
-    }
-
-    // Anchor
-    matchAnchorToHash = () => {
-        return this.anchorUrl && `#${this.anchorUrl}` === document.location.hash;
-    }
-    setAnchor = () => {
-        const hashValue = document.location.hash.substring(1);
-        const item = document.querySelector('[data-anchor="' + hashValue + '"]');
-
-        if (item) {
-            item.scrollIntoView({
-                behavior: 'auto',
-                block: 'center',
-                inline: 'center'
-            });
-
-            item.querySelector('.game-title > a').focus();
-        }
-
-        if (this.matchAnchorToHash()) {
-            this.setState({'anchor': true});
-        }
-    }
-    toggleAnchor = event => {
-        const item = document.querySelector('[data-anchor="' + this.anchorUrl + '"]');
-
-        if (item) {
-            if (this.matchAnchorToHash()) {
-                this.setState({'anchor': false});
-
-                window.history.pushState('', document.title, window.location.pathname);
-
-                event.preventDefault();
-            } else {
-                this.setState({'anchor': true});
-            }
-        }
-    }
-
-    get anchorUrl () {
-        const {url} = this.game;
-
-        if (url) {
-            return url.replace('https://en.wikipedia.org/wiki/', '');
-        }
-
-        return null;
-    }
-    get anchor () {
-        const {url, release} = this.game;
-        const icon = this.state.anchor ? iconLinkOff : iconLink;
-
-        return (url && release) ? (
-            <Anchor
-                href={`#${this.anchorUrl}`}
-                title='Game anchor'
-                onClick={this.toggleAnchor}
-            >
-                <img src={icon} width='24' height='24' alt='anchor' />
-            </Anchor>
-        ) : null
-    }
-
-    // Games
     matchGameToQuarter (index, year, release) {
         if (!release) return true;
 
@@ -164,23 +50,14 @@ class TimelineGames extends Component {
 
                 if (games[year]) {
                     for (const game of games[year]) {
-                        const {anchor} = this.state;
-                        const {title, url, genre, release} = game;
+                        const {title, genre, release} = game;
 
                         if (genreName === genre && this.matchGameToQuarter(index, year, release)) {
-                            this.game.url = url;
-                            this.game.release = release;
-
                             gamesItems.push(
                                 <Item
                                     key={title}
-                                    className={[
-                                        anchor ? 'anchor' : '',
-                                        !release ? 'release-unknown' : ''
-                                    ]}
-                                    data-anchor={release ? this.anchorUrl : null}
+                                    className={!release ? 'release-unknown' : ''}
                                 >
-                                    {this.anchor}
                                     <Game key={title} game={game} />
                                 </Item>
                             );
@@ -197,7 +74,10 @@ class TimelineGames extends Component {
                 }
 
                 result.push(
-                    <div key={index} data-quarter={`Q${index}`}>
+                    <div
+                        key={index}
+                        data-quarter={`Q${index}`}
+                    >
                         {gamesList}
                     </div>
                 );
