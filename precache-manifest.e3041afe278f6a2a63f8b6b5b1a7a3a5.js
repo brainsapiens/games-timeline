8e7f0692b0b9c57341f7c9a4863d0e04930109c2
Vyacheslav Efremenko <vyacheslav.efremenko@finnplay.com>
2020-10-27 15:56:43 +0300

    Updates

---
+++ precache-manifest.e3041afe278f6a2a63f8b6b5b1a7a3a5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff362f8a156f69a08e0e32a2075ae64e",
+    "url": "/games-timeline/index.html"
+  },
+  {
+    "revision": "bc7e9db51bc232708073",
+    "url": "/games-timeline/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "8ce6bd3c23c892b34bc3",
+    "url": "/games-timeline/static/css/main.3de66170.chunk.css"
+  },
+  {
+    "revision": "bc7e9db51bc232708073",
+    "url": "/games-timeline/static/js/2.6852931f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/games-timeline/static/js/2.6852931f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ce6bd3c23c892b34bc3",
+    "url": "/games-timeline/static/js/main.b3c9c816.chunk.js"
+  },
+  {
+    "revision": "d31b4acdfcefc42da775",
+    "url": "/games-timeline/static/js/runtime-main.9f701a10.js"
+  },
+  {
+    "revision": "964500a03612910be1c1f24bbfd70dcb",
+    "url": "/games-timeline/static/media/link.964500a0.svg"
+  }
+]);
