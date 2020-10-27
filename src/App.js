fa6cf9c19532c6a27d8c6967b0819f4c4ca26a7c
Vyacheslav Efremenko <vyacheslav.efremenko@finnplay.com>
2020-10-27 16:01:36 +0300

    Configured Github Pages

--- src/App.js
+++ src/App.js
@@ -7,9 +7,12 @@ import AppHeader from './components/App/Header';
 import AppMain from './components/App/Main';
 import AppFooter from './components/App/Footer';
 
+import global from './global';
+const {app: {basename}} = global;
+
 function App () {
     return (
-        <Router>
+        <Router basename={basename}>
             <AppHeader />
             <AppMain />
             <AppFooter />
--- src/global.js
+++ src/global.js
@@ -1,6 +1,7 @@
 export default {
     app: {
-        title: 'Games Timeline'
+        title: 'Games Timeline',
+        basename: '/games-timeline/'
     },
     pages: {
         about: {
