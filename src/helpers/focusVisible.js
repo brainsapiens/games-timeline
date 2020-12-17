ae48c48a46ddba304b4e8c8d627904f50cd12849
Vyacheslav Efremenko <brainsapiens@gmail.com>
2020-12-17 14:16:17 +0300

    Fix

--- src/helpers/focusVisible.js
+++ src/helpers/focusVisible.js
@@ -1,14 +1,12 @@
 const removeFocusVisible = e => {
-    const html = document.documentElement;
-    html.classList.remove('js-focus-visible');
+    document.documentElement.classList.remove('js-focus-visible');
 
     const target = e.target;
     target.removeEventListener('blur', removeFocusVisible);
 }
 
 const addFocusVisible = el => {
-    const html = document.documentElement;
-    html.classList.add('js-focus-visible');
+    document.documentElement.classList.add('js-focus-visible');
 
     el.focus();
     el.addEventListener('blur', removeFocusVisible);
